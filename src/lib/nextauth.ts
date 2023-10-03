7aa0c13d9ccffd9f9f47efeed3fefd9390b736bb
Yusuf Çolak <yusuf@microprefix.com>
2023-10-03 17:47:13 +0300

    update

--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -1,48 +1,59 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { DefaultSession, NextAuthOptions } from "next-auth";
-import { prisma } from "./db";
 
+import { DefaultSession, NextAuthOptions, getServerSession } from "next-auth";
+import { prisma } from "./db";
+import GoogleProvider from "next-auth/providers/google";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
-       id:String;
+      id: string;
     } & DefaultSession["user"];
   }
 }
+
 declare module "next-auth/jwt" {
   interface JWT {
-    id:string;
-  }}
-
-
+    id: string;
+  }
+}
 export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-     jwt: async ([token]) => {
-      const db_user = await prisma.user.findUnique({
-        where: { email: token?.email },
+    jwt: async ({ token }) => {
+      const db_user = await prisma.user.findFirst({
+        where: {
+          email: token?.email,
+        },
       });
-      if(db_user){
+      if (db_user) {
         token.id = db_user.id;
-        token.name = db_user.name;
-
       }
-return token;
+      return token;
     },
-      session : ({ session, token }) => {
-        if (token) {
-          session.user.id = token.id;
-          session.user.name = token.name;
-          session.user.email = token.email;
-          session.user.image = token.picture;
+    session: ({ session, token }) => {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
 
-        }
       }
-    }
-  secret: process.env.NEXTAUTH_SCRET,
+      return session;
+    },
+  },
   adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
 };
 
+export const getAuthSession = () => {
+  return getServerSession(authOptions);
+};
